ea3465c82a9f57db31d71391a2d36977d264826b
Kamil Politowicz <k.politowicz@gmail.com>
2011-12-10 12:31:19 +0100

    rails 3.1.3 app init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublicApiTestDevise::Application.config.secret_token = 'a9a0fa2a7002234078519a65c115ee59e664712be9520471da9e920d363da117fa0c754c2141c361cfd04f467316b4ea13b9b76996a0623d508acafd88e0c10c'
